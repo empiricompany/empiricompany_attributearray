794ea0ea86f0fd26e384705a338355f0e4d84ead
empiricompany <tonyweboss@gmail.com>
2017-11-15 13:53:06 +0100

    code refactoring localization remove buttons

--- app/code/local/Empiricompany/Attributearray/Model/Custom.php
+++ app/code/local/Empiricompany/Attributearray/Model/Custom.php
@@ -1,28 +1,12 @@
 <?php
-class Empiricompany_Attributearray_Model_Custom extends Mage_Eav_Model_Entity_Attribute_Backend_Abstract{
+class Empiricompany_Attributearray_Model_Custom extends Mage_Eav_Model_Entity_Attribute_Backend_Serialized {
     public function beforeSave($object) 
     {
+        // Reset value
         $attributeCode = $this->getAttribute()->getAttributeCode();
-        $data = $object->getData($attributeCode);
-        //Mage::log($data);
-        if (is_array($data)) {
-            //Clean Values
-            foreach ($data as $key => $value) {
-                if(empty($value[1]) AND empty($value[2])){
-                    unset($data[$key]);
-                }
-            }
-            $object->setData($attributeCode, serialize($data));
+        if( !Mage::app()->getRequest()->getPost('product')[$attributeCode] ){
+            $object->setData($attributeCode, NULL);
         }
         return parent::beforeSave($object);
-    }
-    public function afterLoad($object) {
-        $attributeCode = $this->getAttribute()->getAttributeCode();
-        $data = $object->getData($attributeCode);
-        //Mage::log($data);
-        if (!is_array($data)) {
-            $object->setData($attributeCode, @unserialize($data));
-        }
-        return parent::afterLoad($object);
     }
 }
